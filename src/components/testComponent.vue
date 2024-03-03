fb2f80de3ca432d6183eb0a069be4f90e4cb3254
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2024-03-03 13:30:18 +0300

    guide: for Dima

--- src/components/testComponent.vue
+++ src/components/testComponent.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-import {useI18n} from "vue-i18n";
-const {t} = useI18n({useScope: 'global'})
+
 </script>
 
 <template>
-  <div class='tailer'>
-    {{$t('me.i')}}
+  <div class="box">
+    <div class='tailer'>
+      {{$t('me.i')}}
+    </div>
+    <div class="taino">
+      {{$t('me.name')}}
+    </div>
   </div>
-  <div class="taino">
-    {{$t('me.name')}}
-  </div>
+
 </template>
 
 <style scoped>
