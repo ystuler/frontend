e6b9d29da1079bde61636cc255c3e2c2b5557e72
R10K5 <r10k5.job@gmail.com>
2024-03-15 21:05:54 +0300

    поправила багу

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainView from '@/views/MainView.vue'
+import MainView from '../views/MainView.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        { path: '/', component: MainView },
+        {
+            path: '/',
+            component: MainView
+        },
     ]
 })
 
