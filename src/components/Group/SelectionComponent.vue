65449088dd00cc3eca7745fb35949d123ea86bf4
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2024-03-17 16:35:44 +0300

    style(star): new path

--- src/components/Group/SelectionComponent.vue
+++ src/components/Group/SelectionComponent.vue
@@ -2,18 +2,7 @@
   <div class="grid-container noselect">
     <div class="square">
       <div class="rectangle">
-        <svg class="star" width="45.000000" height="57.000000" viewBox="0 0 25 23" fill="none"
-             xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-          <desc>
-          </desc>
-          <defs/>
-          <path id="Звезда 1"
-                d="M12 -3.06e-5L9.08 8.63L-0.37 8.63L7.27 13.97L4.35 22.61L12 17.27L19.64 22.61L16.72 13.97L24.36 8.63L14.91 8.63L12 -3.06e-5Z"
-                fill="#FFFFFF" fill-opacity="1.000000" fill-rule="nonzero"/>
-          <path id="Звезда 1"
-                d="M9.08 8.63L-0.37 8.63L7.27 13.97L4.35 22.61L12 17.27L19.64 22.61L16.72 13.97L24.36 8.63L14.91 8.63L12 -3.06e-5L9.08 8.63ZM12 3.12L9.79 9.63L2.81 9.63L8.46 13.58L6.27 20.05L12 16.05L17.72 20.05L15.53 13.58L21.18 9.63L14.2 9.63L12 3.12Z"
-                fill="#8A8A8A" fill-opacity="1.000000" fill-rule="evenodd"/>
-        </svg>
+        <div class="star" v-html="starSVG"></div>
 
         <span class="FavoriteGroup">{{ holder }}</span>
       </div>
@@ -76,7 +65,13 @@
   border-top: none;
   border-left: none;
 
-
+  font-size: 24px;
+  font-family: Inter, serif;
+  color: rgb(196, 194, 194);
+  overflow: hidden;
+  white-space: nowrap;
+  padding-left: 1vw;
+  text-overflow: ellipsis;
 
 }
 .forScroll {
@@ -151,6 +146,7 @@ text {
 }
 </style>
 <script setup lang="ts">
+import starSVG from "../../utils/StarSVG.ts";
 import {useGroupStore} from "../../stores/group.store.ts";
 import {computed, ref} from "vue";
 
---
+++ src/utils/StarSVG.ts
@@ -0,0 +1,16 @@
+const starSVG = `
+<svg class="star" width="35.000000" height="55.000000" viewBox="0 0 25 23" fill="none"
+     xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <desc>
+  </desc>
+  <defs/>
+  <path id="Звезда 1"
+        d="M12 -3.06e-5L9.08 8.63L-0.37 8.63L7.27 13.97L4.35 22.61L12 17.27L19.64 22.61L16.72 13.97L24.36 8.63L14.91 8.63L12 -3.06e-5Z"
+        fill="#FFFFFF" fill-opacity="1.000000" fill-rule="nonzero"/>
+  <path id="Звезда 1"
+        d="M9.08 8.63L-0.37 8.63L7.27 13.97L4.35 22.61L12 17.27L19.64 22.61L16.72 13.97L24.36 8.63L14.91 8.63L12 -3.06e-5L9.08 8.63ZM12 3.12L9.79 9.63L2.81 9.63L8.46 13.58L6.27 20.05L12 16.05L17.72 20.05L15.53 13.58L21.18 9.63L14.2 9.63L12 3.12Z"
+        fill="#8A8A8A" fill-opacity="1.000000" fill-rule="evenodd"/>
+</svg>
+`;
+
+export default starSVG;
