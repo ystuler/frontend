77083ae1f50cf38acc19aa043e5b7bb6bc9f7146
Stepanov Mikhail <volodya-stepanov-68@mail.ru>
2024-03-22 18:45:36 +0300

    refactor(group): add classes

--- src/components/Group/GroupMenuComponent.vue
+++ src/components/Group/GroupMenuComponent.vue
@@ -23,12 +23,12 @@ const classes = ref ("Институт")
 
         </li>
       </ul>
-      <div>
+      <div class="fast-classes">
         <span>{{classes}}</span>
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
+        <div class="circle-classes">1</div>
+        <div class="circle-classes">2</div>
+        <div class="circle-classes">3</div>
+        <div class="circle-classes">4</div>
 
       </div>
     </div>
@@ -36,6 +36,7 @@ const classes = ref ("Институт")
 </template>
 
 <style scoped>
+
 .grid-container {
   padding-top: 4vh;
 
@@ -51,9 +52,12 @@ const classes = ref ("Институт")
   padding: 2vw;
   text-align: center;
   height: 3vw;
+  font-size: 0.9vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  font-weight: 600;
+
 }
 
 .background {
@@ -66,9 +70,27 @@ const classes = ref ("Институт")
 .fast-group {
   padding-top: 5vh;
   color: rgb(53, 53, 53);
-  font-size: 20px;
+  font-size: 1.2vw;
   font-weight: 400;
   line-height: 24px;
+
+
+}
+.fast-classes {
+  display: flex;
+  align-items: center;
+  padding-top: 3vh;
+}
+
+.circle-classes {
+  width: 30px;
+  height: 30px;
+  background-color: #3498db;
+  border-radius: 50%;
+  margin: 0.5vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
 }
 
@@ -76,7 +98,7 @@ const classes = ref ("Институт")
   padding-top: 5vh;
 
   color: rgb(53, 53, 53);
-  font-size: 36px;
+  font-size: 2vw;
   font-weight: 700;
   line-height: 44px;
 }
